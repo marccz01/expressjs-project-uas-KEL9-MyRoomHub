1db2bd715319dfc1ce34565a1059021663df6aa2
marccz01 <nelson.marc5678@gmail.com>
2025-12-13 18:30:40 +0700

    uas

---
+++ route/api.js
@@ -0,0 +1,10 @@
+import express from "express";
+import * as userController from "../controllers/userController.js";
+
+
+const api = express.Router()
+
+api.post ("/signup", userController.signUp)
+api.post("/signin", userController.signIn)
+
+export default api
